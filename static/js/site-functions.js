e152010ec3cde67021a59fa07fae7c781f786340
Steven Tammen <stevenwtammen@gmail.com>
2018-04-22 15:36:07 -0400

    Add table of contents (CSS, js, nav <div>)

---
+++ static/js/site-functions.js
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------*/
+
+  // Create Table of Content (invoking Toc.init and then scrollspy)
+
+$(function() {
+  var navSelector = '#toc';
+  var $myNav = $(navSelector);
+  Toc.init($myNav, { headings: 'h1,h2,h3,h4,h5,h6' } );
+  $('body').scrollspy({
+    target: navSelector,
+    offset: 220
+  });
+});
+
+/*---------------------------------------------------------------------------*/
+
+/* Dynamic management of the Table of Content */
+
+$(window).on('load resize scroll', function () {
+/* Auto-move the table of content scroll div while scrolling the main window */
+  var scrollPos = $("div.sticky").offset().top / $(document.body).prop('scrollHeight'); // scrolling of the main window (between 0 and 1)
+  switch(true) { // this is a discrete function to calculate the most appropriate scroll percentage of the ToC div
+    case (scrollPos < 0.2): scrollPos=0; break;
+    case (scrollPos < 0.3): scrollPos=scrollPos * 1.2; break;
+    case (scrollPos > 0.3): scrollPos=scrollPos * 1.4; break;
+    case (scrollPos > 0.6): scrollPos=scrollPos * 1.5; break;
+    case (scrollPos > 0.75): scrollPos=1; break;
+  }
+  $("div.sticky").scrollTop(scrollPos * ($("div.sticky").prop('scrollHeight') - $("div.sticky").innerHeight()));
+/* Dynamically set bottom position of div.sticky only when toc overflows to avoid conflicts between box-shadow and scrollbar */
+if ($(window).height() < $('nav[data-toggle="toc"]').height() + $("div.sticky").position().top)
+  $("div.sticky").css("bottom","0")
+else
+  $("div.sticky").css("bottom","unset");
+})
